4d4f0ccb74781ce779a8ba44c184da2c9fb46ed0
aknyemi <akinyemiayomide98@gmail.com>
2025-03-14 15:06:33 -0700

    'new'

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -6,18 +6,18 @@ interface PlayerProps {
   id: number;
   link: string;
   name: string;
-  title:string;
+  title: string;
   position: string;
 }
 
-const Player: React.FC<PlayerProps> = ({ id, link, name, title , position }) => {
+const Player: React.FC<PlayerProps> = ({ id, link, name, title, position }) => {
   return (
     <Link to={`/details/${id}`}>
       <div className="rounded-lg overflow-hidden">
         <ReactPlayer url={link} controls width="100%" className="rounded-lg" />
         <h2 className="mt-2 text-xl font-bold">{title}</h2>
         <div className="flex justify-between items-center mt-2">
-          <p className='text-orange-300 text-xs'>{position}</p>
+          <p className="text-orange-300 text-xs">{position}</p>
           <p className="mt-2 text-xs">{name}</p>
         </div>
       </div>
