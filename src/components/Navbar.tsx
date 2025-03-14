baa6306945b2ed8eea94cc019c9abc9e2fce4c68
aknyemi <akinyemiayomide98@gmail.com>
2025-03-14 14:45:44 -0700

    'nav'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,37 +9,39 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ toggle }) => {
   return (
     <div className="fixed top-0 left-0 w-full z-50">
-      <div className="bg-black flex justify-between items-center px-3 md:px-10 py-3">
-        <div className="flex items-center">
-          <Link to="/">
-            <img src={logo1} alt="Company Logo" className="h-10" />
-          </Link>
-        </div>
-        <div className="flex">
-          <button
-            onClick={toggle}
-            aria-label="Toggle Sidebar"
-            className="md:hidden"
-          >
-            <AlignJustify className="w-6 h-6 text-white mr-2" />
-          </button>
-          <motion.div
-            initial={{ y: 0 }}
-            animate={{ y: [0, -10, 0] }} // Moves up & down
-            transition={{
-              duration: 0.5,
-              repeat: Infinity,
-              repeatType: 'reverse',
-            }}
-            className="hidden md:block"
-          >
-            <Link
-              to="/rate"
-              className="text-orange-300 hover:bg-orange-300 hover:py-2 hover:px-4 hover:text-white hover:rounded-full transition-all"
-            >
-              HIRE ME
+      <div>
+        <div className="bg-black flex justify-between items-center px-3 md:px-10 py-3">
+          <div className="flex items-center">
+            <Link to="/">
+              <img src={logo1} alt="Company Logo" className="h-10" />
             </Link>
-          </motion.div>
+          </div>
+          <div className="flex">
+            <button
+              onClick={toggle}
+              aria-label="Toggle Sidebar"
+              className="md:hidden"
+            >
+              <AlignJustify className="w-6 h-6 text-white mr-2" />
+            </button>
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: [0, -10, 0] }} // Moves up & down
+              transition={{
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: 'reverse',
+              }}
+              className="hidden md:block"
+            >
+              <Link
+                to="/rate"
+                className="text-orange-300 hover:bg-orange-300 hover:py-2 hover:px-4 hover:text-white hover:rounded-full transition-all"
+              >
+                HIRE ME
+              </Link>
+            </motion.div>
+          </div>
         </div>
       </div>
       <motion.div
@@ -50,7 +52,7 @@ const Navbar: React.FC<NavbarProps> = ({ toggle }) => {
           repeat: Infinity,
           repeatType: 'reverse',
         }}
-        className="absolute top-96 right-5 md:hidden bg-orange-300 py-2 px-4 rounded-full "
+        className="fixed bottom-5 right-5 md:hidden bg-orange-300 py-2 px-4 rounded-full"
       >
         <Link
           to="/rate"
